5ea6da3bdebe5ac8a5a46d6a362ef20895c6e011
Adja Coura Ndour <adja-coura.ndour@ism.edu.sn>
2024-12-26 22:28:59 +0000

    gestion commande

--- gestion_commande/obj/Debug/net8.0/gestion_commande.AssemblyInfo.cs
+++ gestion_commande/obj/Debug/net8.0/gestion_commande.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gestion_commande")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4058dbfa7ec61146e55f4039c803b9dfddbc331d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b03cdb4bdadab7442c5bf7f97a0ecc93d48310b")]
 [assembly: System.Reflection.AssemblyProductAttribute("gestion_commande")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gestion_commande")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
